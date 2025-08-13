2a6d7036fa60330971fc633febc53335970d49e9
Jon.Spahiu <jonspahiu@gmail.com>
2025-08-13 08:53:33 +0200

    13.08

---
+++ 03_select/02_select.sql
@@ -0,0 +1,26 @@
+\! cls
+
+--  02 SELECT
+
+-- basic
+-- SELECT 
+--     *
+-- FROM boo.contacts
+-- ;
+
+
+-- SELECT 
+--     id,
+--     email
+-- FROM boo.contacts
+-- ;
+
+SELECT 
+    id,
+    email,
+    city
+FROM boo.contacts
+WHERE city = 'Berlin'
+;
+
+
